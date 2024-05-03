d284ff3cf19ff2b4d7d36da571feab19556c47b3
antoniofilippa94@gmail.com <antoniofilippa94@gmail.com>
2024-05-03 11:47:24 +0200

    [END]

---
+++ snack2/js/main.js
@@ -0,0 +1,38 @@
+//CREO DUE VARIABILI DATE DALL'UTENTE TRAMITE PROMPT
+
+
+
+
+let parola1 = prompt('inserisci parola');
+
+
+let parola2 = prompt('inserisci parola');
+
+//CREO ARRAY PER CALCOLARE LUNGHEZZA DELLE STRINGHE
+
+
+for (let i = 0; i < parola1.length; i++) {
+}
+
+for (let i2 = 0; i2 < parola2.length; i2++) {
+}
+
+
+
+//MISURO I E I2 E DO RISULTATO FINALE   
+
+
+
+if(i>i2){
+  
+    console.log(parola2);
+  
+    console.log(parola1);
+
+}else{
+
+    console.log(parola1);
+
+    console.log(parola2);
+
+}
