7e0a0a6ba3f1b0a26d0820badd58ab326fc43ae0
antoniofilippa94@gmail.com <antoniofilippa94@gmail.com>
2024-05-03 15:27:28 +0200

    [FINE]

---
+++ snack9/js/main.js
@@ -0,0 +1,22 @@
+// Somma dei primi 10 numeri naturali
+"Use strict"
+
+let somma = 0;
+
+for (let i = 1; i <= 10; i++) {
+
+    somma += i;
+
+}
+
+
+// Media dei primi 10 numeri naturali
+
+let media = somma / 10;
+
+
+// STAMPO RISULTATI IN CONSOLE
+
+console.log("Somma dei primi 10 numeri:", somma);
+
+console.log("Media dei primi 10 numeri:", media);
